5e61bf7efac35106e92a58c69ce539c3810c0ead
Garik9402 <gbesati@mail.ru>
2022-12-04 12:52:03 +0300

    in the process;)

--- script.js
+++ script.js
@@ -1,130 +1,31 @@
-// // // const objectItems = [{
-// // //    title: 'привет',
-// // //    price: 28,
-// // //    color: 'blue',
-// // // },
-// // // {
-// // //    title: 'рад',
-// // //    price: 40,
-// // //    color: 'red',
-// // // },
-// // // {
-// // //    title: 'шалом',
-// // //    price: 60,
-// // //    color: 'yellow',
-// // // },
-// // // ];
-
-// // // function funcObj(s) {
-// // //    let titleNode = document.querySelector('.header__titles')
-// // //    let priceNode = document.querySelector('.header__price')
-// // //    let colorNode = document.querySelector('.header__colors')
-// // //    s.forEach(el => {
-// // //       let pt = document.createElement('p')
-// // //       let p = document.createElement('p')
-// // //       let div = document.createElement('div')
-// // //       pt.classList.add('title')
-// // //       p.classList.add('price')
-// // //       div.classList.add('color')
-// // //       div.style.background = el.color
-// // //       div.addEventListener('click', function () {
-// // //          titleNode.innerHTML = el.title
-// // //          priceNode.innerHTML = el.price
-// // //       })
-// // //       titleNode.append(pt)
-// // //       priceNode.append(p)
-// // //       colorNode.append(div)
-// // //    });
-// // // }
-// // // funcObj(objectItems)
-// // // async function asincF(w) {
-// // //    let fuc = await (w + 4)
-// // //    console.log(fuc)
-// // // }
-// // // asincF(6)
 
 
-// // const inputsNode = document.querySelectorAll('.header__input')
-// // const buttonNode = document.querySelector('.header__button')
-// // const inputNode = document.getElementById('result')
-// // function funcRes() {
-// //    if (inputNode.value !== 0) {
-// //       let str = inputNode.value;
-// //       let arr = str.split(' ');
-// //       for (let i = 0; i < arr.length; i++) {
-// //          // inputsNode[f].value = arr[i]
-// //          console.log(arr[i])
-// //       }
-// //    }
-// //    // if (inputNode.value == 0) {
-// //    //    inputNode.placeholder = 'введите ваши данные'
-// //    //    for (var f = 0; f < inputsNode.length; f++) {
-// //    //       inputsNode[f].value = ''
-// //    //    }
-// //    // }
-
-// // }
-
-// // inputNode.addEventListener('blur', funcRes)
-// const obj = [{
-//    name: 'Гарик',
-//    fam: 'Бесаев',
-//    otch: 'Сосланович',
-//    age: '28 лет',
-// },
-// {
-//    name: 'Игорь',
-//    fam: 'Меняев',
-//    otch: 'Игоревич',
-//    age: '18 лет',
-// },
-// {
-//    name: 'Артур',
-//    fam: 'Хитяев',
-//    otch: 'Виняевич',
-//    age: '43 лет',
-// }
-// ]
-let colorsStr = ['один', 'два', 'три']
-let productNode = document.querySelectorAll('.product')
-
-// function funcInp() {
-//    const inputNode = document.querySelector('.header__input')
-//    let inpV = inputNode.value
-//    let res = inpV.split(' ')
-//    let resu = res.join(',')
-//    paragrafNOde.innerHTML = resu
-
-// }
-// funcInp()
-let paragrafNOde = document.querySelectorAll('.para')
-paragrafNOde.forEach(el => {
-
+const src = document.querySelectorAll(".button")
+let clientY
+window.addEventListener('touchmove', (e) => {
+   src.forEach(src => {
+      if (src.contains(e.target)) {
+         let parent = src.closest('.button-w')
+         clientY = e.touches[0].clientY;
+         parent.style.top = clientY + 'px'
+         let dpx = 0
+         if ((clientY < dpx)) {
+         }
+         let hg = 200
+         if ((clientY > 200)) {
+            parent.classList.add('button-w--transform')
+         }
+      }
+   })
+   console.log(clientY)
 })
 
-const src = document.querySelector(".button");
-let clientX;
-let clientY;
 
-src.addEventListener('touchstart', (e) => {
-   // Cache the client X/Y coordinates
-   clientX = e.touches[0].clientX;
-   clientY = e.touches[0].clientY;
-}, false);
 
-src.addEventListener('touchend', (e) => {
-   let deltaX;
-   let deltaY;
 
-   // Compute the change in X and Y coordinates.
-   // The first touch point in the changedTouches
-   // list is the touch point that was just removed from the surface.
-   deltaX = e.changedTouches[0].clientX - clientX;
-   deltaY = e.changedTouches[0].clientY - clientY;
-   console.log(deltaX)
 
-   // Process the data…
-}, false);
+
+
 
 
 
